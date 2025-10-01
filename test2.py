6a5a549f20afc2fb5ce2672f8aeed38c1c6374b0
Haotian <haotianzh@uconn.edu>
2025-10-01 19:34:39 -0400

    add bootstrap

--- test2.py
+++
@@ -1,9 +0,0 @@
-from phytreeviz import TreeViz, load_example_tree_file
-import popgen
-
-nwk = '(((1)2,2)2:3,(3,4)4:2)5:1;'
-# nwk = popgen.utils.get_random_binary_tree(100).output()
-tv = TreeViz(nwk)
-tv.show_branch_length()
-tv.show_confidence()
-tv.savefig("api_example01.png", dpi=300)
